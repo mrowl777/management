f77f665c0d9bef7eb3389df63f92708851cb5a8b
mrowl <wastedowl777@gmail.com>
2019-05-02 14:03:24 +0300

    reinit

--- base_page/js/database.js
+++ base_page/js/database.js
@@ -22,7 +22,6 @@ function delete_panel_usr(e) {
 }
 
 function add_staff(e) {
-    console.log('clicked');
     var name = $('.name').val();
     var surname = $('.surname').val();
     var last_name = $('.last_name').val();
@@ -57,4 +56,5 @@ function show_form(){
     var r_part_8 = '</table>';
     var render = r_part_0+r_part_1 +r_part_2+r_part_3+r_part_4+r_part_5+r_part_6+r_part_7+r_part_8;
     $(this).replaceWith(render);
+    init_events();
 }
