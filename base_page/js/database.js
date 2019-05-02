409877917fe9c125c27519c41f9ac67972013beb
mrowl <wastedowl777@gmail.com>
2019-05-02 13:44:17 +0300

    r_part_0

--- base_page/js/database.js
+++ base_page/js/database.js
@@ -25,6 +25,7 @@ function on_base_answer(data) {
 }
 
 function show_form(){
+    var r_part_0 = '<table border="1" class="database_table">';
     var r_part_1 = '<tr class="database_tr" id="new">';
     var r_part_2 = "<td><input type='text' class='new_pass_form' placeholder='введите имя '></input></td>";
     var r_part_3 = "<td><input type='text' class='new_pass_form' placeholder='введите отчество '></input></td>";
@@ -32,6 +33,7 @@ function show_form(){
     var r_part_6 = '<td title="Добавить" class="delete_panel_user"><i class="icon-ok icon-large"></i></td>';
     var r_part_5 = '<td><td>';
     var r_part_7 = '</tr> ';
-    var render = r_part_1 +r_part_2+r_part_3+r_part_4+r_part_5+r_part_6+r_part_7;
+    var r_part_8 = '</table>';
+    var render = r_part_0+r_part_1 +r_part_2+r_part_3+r_part_4+r_part_5+r_part_6+r_part_7+r_part_8;
     $(this).replaceWith(render);
 }
