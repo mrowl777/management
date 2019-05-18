21f8c85319a7321f7fe24c4e1bda5fabc97c8dfa
mrowl <wastedowl777@gmail.com>
2019-05-18 20:16:11 +0300

    _date

--- components/MainController.php
+++ components/MainController.php
@@ -114,7 +114,7 @@ class MainController extends db_handler {
     function dates_builder( $_date ){
         $dates = explode( ":", $_date );
         if(empty($dates)){
-            $dates[] = $date;
+            $dates[] = $_date;
         }
         $cur_time = time();
         $counter = 0;
