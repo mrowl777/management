42f6d0c3b1c4135506df15094b9403c6c785a20f
mrowl <wastedowl777@gmail.com>
2019-05-18 20:29:44 +0300

    echo

--- components/MainController.php
+++ components/MainController.php
@@ -134,7 +134,7 @@ class MainController extends db_handler {
         }
 
         if($length < 30){
-            die( var_dump($length)) . "<br/>";
+            echo( var_dump($length)) . "<br/>";
             $iterations = 30 - $length;
             
             for($i=0;$i<$iterations;$i++){
