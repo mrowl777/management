c3709814e5a1ef355cd946cf4c1f94f33e836feb
mrowl <wastedowl777@gmail.com>
2019-05-18 16:23:15 +0300

    []

--- components/MainController.php
+++ components/MainController.php
@@ -65,8 +65,7 @@ class MainController extends db_handler {
         }
 
         if( !empty($username) && $username != '' ){
-            die($username);
-            $DATA['new_user'] = [
+            $DATA['new_user'][] = [
                 'login' => $username,
                 'password' => $pass
             ];
