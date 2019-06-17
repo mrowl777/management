9d0cb711ad3ab4e483e709d8ae91c1b7038834bb
mrowl <wastedowl777@gmail.com>
2019-06-18 02:13:05 +0300

    s

--- components/MainController.php
+++ components/MainController.php
@@ -253,14 +253,14 @@ class MainController extends db_handler {
         $DATA['header_left'] = 'not_required';
         $DATA['header_right'] = '';
 
-        if(!$this->_on_get_rights_usr()){
-            $type = $this->get_user_time_type( $_COOKIE['user_name'] );
-            if($type == 1){
-                $DATA['header_left'] = 'not_required';
-            }else{
-                $DATA['header_right'] = 'not_required';
-            }
-        }
+        // if(!$this->_on_get_rights_usr()){
+        //     $type = $this->get_user_time_type( $_COOKIE['user_name'] );
+        //     if($type == 1){
+        //         $DATA['header_left'] = 'not_required';
+        //     }else{
+        //         $DATA['header_right'] = 'not_required';
+        //     }
+        // }
         
         $DATA['times'] = $time_var;
         $DATA['is_admin'] = $this->_on_get_rights_usr();
