f2f017bc2eac430802a7fbc883f09408ae6c31d1
mrowl <wastedowl777@gmail.com>
2019-06-18 01:27:55 +0300

    `

--- components/MainController.php
+++ components/MainController.php
@@ -270,8 +270,8 @@ class MainController extends db_handler {
         $l_counter = 0;
         $r_counter = 0;
         $is_req = false;
-        $hidden_left = false;
-        $hidden_right = false;
+        // $hidden_left = false;
+        // $hidden_right = false;
         foreach($uids as $id){
             if(isset($staff_list[$id])){
                 $time = $staff_list[$id]['time'];
