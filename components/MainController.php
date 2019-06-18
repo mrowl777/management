5627c9e13f1352e2fa42e0fda70d3c8889b1873a
mrowl <wastedowl777@gmail.com>
2019-06-18 14:42:18 +0300

    title

--- components/MainController.php
+++ components/MainController.php
@@ -30,8 +30,8 @@ class MainController extends db_handler {
 
         if(!$is_admin){
             $obj = $this->get_user_data( $_COOKIE["user_name"] );
-            die(var_dump($obj));
             $title = $obj['first_name'] . ' ' . $obj['surname'];
+            die(var_dump($title));
         }
 
         $DATA['name'] = $title;
